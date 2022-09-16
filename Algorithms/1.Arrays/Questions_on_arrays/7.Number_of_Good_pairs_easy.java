24650fa57fbfd055da6afb39282db36df4c0782f
Ayush-kathayat <ayushkathayat789@gmail.com>
2022-09-16 20:45:25 +0530

    added question

---
+++ Algorithms/1.Arrays/Questions_on_arrays/7.Number_of_Good_pairs_easy.java
@@ -0,0 +1,22 @@
+
+//link to the question --> https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums)
+    {
+        
+        int cnt = 0  ;
+        
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            for(int j = (i+1) ; j < nums.length ; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
